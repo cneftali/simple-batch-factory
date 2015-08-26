270dd98f653c88b428b696c6f266e3795781fbde
cneftali <cneftali@emailvision.com>
2015-08-26 14:10:56 +0200

    add  sample engine slave

---
+++ schedule-service/src/test/java/com/github/cneftali/job/schedule/rest/EngineStubResource.java
@@ -0,0 +1,24 @@
+package com.github.cneftali.job.schedule.rest;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import java.util.Date;
+
+import com.github.cneftali.job.commons.batch.JobLaunchRequest;
+import com.github.cneftali.job.commons.batch.JobLaunchingResponse;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/process")
+public class EngineStubResource {
+
+    @RequestMapping(method = POST, consumes = "application/json")
+    public JobLaunchingResponse create(@RequestBody final JobLaunchRequest request) {
+        return new JobLaunchingResponse(request.getCreateTime().toDate().getTime(),
+                                        new Date().getTime(),
+                                        1,
+                                        request.getJobName());
+    }
+}
